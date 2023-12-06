117439ba56558d70b6fe3118d32fc7a55e1385c5
bvbssk07 <153093418+bvbssk07@users.noreply.github.com>
2023-12-06 14:52:47 +0000

    Add files via upload
    final

--- # backend.py
+++ # backend.py
@@ -6,7 +6,7 @@ import requests
 app = Flask(__name__)
 
 # Remplacez 'YOUR_GITHUB_API_KEY' par votre clé d'API GitHub
-GITHUB_API_KEY = 'YOUR_GITHUB_API_KEY'
+GITHUB_API_KEY = 'https://github.com/bvbssk07/Projet-API.git'
 
 @app.route('/user/<username>', methods=['GET'])
 def get_user_profile(username):
@@ -45,3 +45,54 @@ def get_user_profile(username):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# frontend.py
+
+import tkinter as tk
+from tkinter import messagebox
+import requests
+
+def get_github_profile(username):
+    backend_url = f'http://localhost:5000/user/{username}'
+
+    try:
+        response = requests.get(backend_url)
+        response.raise_for_status()  # Gérer les erreurs HTTP
+
+        profile_data = response.json()
+
+        # Afficher les détails du profil récupéré
+        messagebox.showinfo(
+            'GitHub Profile',
+            f"Username: {profile_data['login']}\n"
+            f"Avatar: {profile_data['avatar_url']}\n"
+            f"Repositories: {profile_data['public_repos']}\n"
+            f"Followers: {profile_data['followers']}\n"
+            f"Following: {profile_data['following']}"
+        )
+
+    except requests.exceptions.RequestException as e:
+        # Informer l'utilisateur en cas d'échec de la requête
+        messagebox.showerror('Error', f"Failed to retrieve GitHub profile: {str(e)}")
+
+def on_submit():
+    username = entry.get()
+    if username:
+        get_github_profile(username)
+    else:
+        messagebox.showwarning('Warning', 'Please enter a GitHub username.')
+
+# Interface utilisateur
+root = tk.Tk()
+root.title('GitHub Profile Viewer')
+
+label = tk.Label(root, text='Enter GitHub username:')
+label.pack(pady=10)
+
+entry = tk.Entry(root)
+entry.pack(pady=10)
+
+submit_button = tk.Button(root, text='Submit', command=on_submit)
+submit_button.pack(pady=10)
+
+root.mainloop()
